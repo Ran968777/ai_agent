2f195c54a35dc637aa3e37767e355a9946ecf976
frankran <ranjiansong@foxmail.com>
2026-02-09 22:59:40 +0800

    自定义file_saver

--- app/code_agent/tools/file_saver.py
+++ app/code_agent/tools/file_saver.py
@@ -1,5 +1,6 @@
 import json
 from collections.abc import Sequence
+from pathlib import Path
 from typing import Any, Self
 
 from langchain_core.runnables import RunnableConfig
@@ -14,17 +15,14 @@ from app.code_agent.tools.file_tools import file_toos
 
 
 class FileSaver(BaseCheckpointSaver[str]):
-    def __init__(self, base_path: str = "/Users/ranjiansong/Desktop/skai/py_project/ai_agent/.temp/checkpoint") -> None:
+    def __init__(self, base_path: str = "/Users/ranjiansong/Desktop/Doc/python/ai-agent/.temp/checkpoint") -> None:
         super().__init__()
         self.base_path = base_path
 
         os.makedirs(self.base_path, exist_ok=True)
 
-    def get_tuple(self, config: RunnableConfig) -> CheckpointTuple | None:
-        pass
-
     def _get_checkpoint_path(self, thread_id: str, checkpoint_id: str):
-        dir_path = os.path.join(self.base_path, thread_id, checkpoint_id)
+        dir_path = os.path.join(self.base_path, thread_id)
         os.makedirs(dir_path, exist_ok=True)
         file_path = os.path.join(dir_path, checkpoint_id + ".json")
         return file_path
@@ -34,6 +32,43 @@ class FileSaver(BaseCheckpointSaver[str]):
         pickled = pickle.dumps(data)
         return base64.b64encode(pickled).decode("utf-8")
 
+    def _deserialize_data(self, data: str) -> Any:
+        import pickle, base64
+        decoded = base64.b64decode(data)
+        return pickle.loads(decoded)
+
+    def get_tuple(self, config: RunnableConfig) -> CheckpointTuple | None:
+        # 找到正确的checkpoint文件路径
+        thread_id = config["configurable"]["thread_id"]
+        # # checkpoint_id = config["configurable"].get("checkpoint_id")
+        #
+        # 读取文件内容
+        dir_path = os.path.join(self.base_path, thread_id)
+        checkpoint_files = list(Path(dir_path).glob("*.json"))
+        checkpoint_files.sort(key=lambda x: x.stem, reverse=True)
+        latest_checkpoint = checkpoint_files[0]
+        checkpoint_id = latest_checkpoint.stem
+        checkpoint_path = self._get_checkpoint_path(thread_id, checkpoint_id)
+
+        # 解析文件内容，反序列化
+        with open(checkpoint_path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        checkpoint = self._deserialize_data(data["checkpoint"])
+        metadata = self._deserialize_data(data["metadata"])
+        # 返回checkpoint对象
+
+        return CheckpointTuple(
+            config={
+                "configurable": {
+                    "thread_id": thread_id,
+                    "checkpoint_id": checkpoint_id,
+                }
+            },
+            checkpoint=checkpoint,
+            metadata=metadata,
+        )
+
     def put(
             self,
             config: RunnableConfig,
@@ -41,25 +76,20 @@ class FileSaver(BaseCheckpointSaver[str]):
             metadata: CheckpointMetadata,
             new_versions: ChannelVersions,
     ) -> RunnableConfig:
-        # 生成存储的文件路径
-        thread_id = config["configurable"]["thread_id"]
+        thread_id = str(config["configurable"]["thread_id"])
         checkpoint_id = checkpoint["id"]
         checkpoint_path = self._get_checkpoint_path(thread_id, checkpoint_id)
 
-        # 将checkpoint 序列化为json
         checkpoint_data = {
             "checkpoint": self._serialize_data(checkpoint),
             "metadata": self._serialize_data(metadata),
         }
 
-        # 存储到文件
         with open(checkpoint_path, "w", encoding="utf-8") as f:
             json.dump(checkpoint_data, f, indent=2, ensure_ascii=False)
-            # f.write(self._serialize_data(checkpoint_data))
 
-        # 生成返回值
         return {
-            "configurable":{
+            "configurable": {
                 "thread_id": thread_id,
                 "checkpoint_id": checkpoint_id,
             }
